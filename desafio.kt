7242885cbb88349da58853c05e0dc412c68b35c5
Rodrigo Moura de Carvalho <65140429+C4rv4lh098@users.noreply.github.com>
2023-05-22 23:12:57 -0400

    Realizando o desafio Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, var email: String){
+    override fun toString(): String = "Nome: $nome, Email: $email"
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60, val nivel: Nivel){
+    override fun toString(): String = "Nome: $nome, Duração: $duracao horas, Nível: $nivel"
+}
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario){
+        inscritos.add(usuario)
     }
 }
 
+fun mostrarFormacao(curso: Formacao){
+    println("\nDescrição: " + curso.component1())
+    curso.component2().forEach(){
+        conteudo -> println("$conteudo")
+    }
+    
+}
+
+fun mostrarInscritos(curso: Formacao){
+    println("\nAlunos: " + curso.nome)
+    curso.inscritos.forEach(){
+        alunos -> println("$alunos")
+    }
+    
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo = ConteudoEducacional("Doker Fundamentals", nivel = Nivel.INTERMEDIARIO)
+    val conteudo1 = ConteudoEducacional("Dominando a linguagem de Programação Java", 120, Nivel.BASICO) 
+    val conteudo2 = ConteudoEducacional("Dominando a linguagem de Programação Kotlin", 120, Nivel.BASICO)
+    val conteudo3 = ConteudoEducacional("Primeiros Passos com SQL", 30, Nivel.BASICO)
+    val conteudo4 = ConteudoEducacional("Refinando Sua Técnica com desafios de Código em Java", 30, Nivel.INTERMEDIARIO)
+    val conteudo5 = ConteudoEducacional("Refinando Sua Técnica com desafios de Código em Kotlin", nivel = Nivel.INTERMEDIARIO)
+    val conteudo6 = ConteudoEducacional("Explorando o Spring Framwork com Java e Kotlin", 200 , Nivel.AVANCADO)
+    
+    val formacao = Formacao("Bootcamp TQI kotlin - Backend Developer", mutableListOf(conteudo, conteudo1, conteudo2, conteudo3))
+    val user = Usuario("Rodrigo Moura", "rodrigo.carvalho.1998@gmail.com")
+    val user1 = Usuario("Venilton FalvoJr","venilto.falvojr@dio.com")
+    
+    formacao.matricular(user)
+    formacao.matricular(user1)
+    mostrarFormacao(formacao)
+    mostrarInscritos(formacao)
+    
+    var formacao1 = Formacao("Bootcamp TQI kotlin - 2º Edição", mutableListOf(conteudo, conteudo1, conteudo4, conteudo2, conteudo5, conteudo3, conteudo6))
+    formacao1.matricular(user)
+    mostrarFormacao(formacao1)
+    mostrarInscritos(formacao1)
 }
